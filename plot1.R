9b2f3d40060c0eb7ff26893926d80b2f486ced17
premziom <32780163+premziom@users.noreply.github.com>
2017-11-11 18:19:23 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-
+#downloading zip file  
 url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 zipfile='project_w1_data.zip'
 file='household_power_consumption.txt'
@@ -7,13 +7,15 @@ if (!file.exists(zipfile)){
   url
   download.file(url,zipfile)
 }  
+#unzipping data file
 if (!file.exists(file)) { 
   unzip(zipfile) 
 }
-
+#reading data and subsetting
 dataIn<-read.table(file,header=TRUE,sep=";",na.strings = "?")
 dataIn<-subset(dataIn,dataIn$Date %in% c("1/2/2007","2/2/2007"))
 
+#changing variables to date and time
 dataIn$Time<-strptime(paste(as.character(dataIn$Date)," ",as.character(dataIn$Time),sep=""),format="%d/%m/%Y %H:%M:%S")
 
 dataIn$Date<-as.Date(dataIn$Date[1],format="%d/%m/%Y")
